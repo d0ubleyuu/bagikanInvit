7306dd29b86ac6f42f69381c40595cf4ebcfeecd
Wahyu Siregar <wahyuusiregarr@gmail.com>
2025-01-13 16:54:07 +0700

    Commit Code first

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/**/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      Cormorant: ["Cormorant Upright", "serif"],
+      Lato: ["Lato", "sans-serif"],
+      Arabic: ["IBM Plex Sans Arabic", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
